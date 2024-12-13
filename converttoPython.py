bfed3a93a5b390cc35311faf1fe6088957e1d798
Brayden O'Neal <71409691+braydenoneal@users.noreply.github.com>
2024-12-12 17:43:55 -0800

    Update converttoPython.py

--- converttoPython.py
+++ converttoPython.py
@@ -1,14 +1,30 @@
 from scipy.io import loadmat
-import h5py
-import numpy as np 
 
-Hand_Close = 'Hand_Close-1.mat'
-data = loadmat(Hand_Close)
+gestures = [
+    'Hand_Close',
+    'Index',
+    'Index_Little',
+    'Peace',
+    'Radial_Deviation',
+    'Right_Angle',
+    'Thumb_Little',
+    'Thumb_UP',
+    'Ulner_Deviation',
+    'Wrist_Extension',
+    'Wrist_Flexion',
+]
 
+orientations = [
+    'pronation',
+    'rest',
+    'supination',
+]
 
-# Print variable names and data
-print(data.keys())
-print(data['__header__'], data['__version__'], data['__globals__'], data['value'])
+for i in range(19):
+    for orientation in orientations:
+        for gesture in gestures:
+            for j in range(5):
+                path = f'FORS-EMG/Subject{i + 1}/{orientation}/{gesture}-{j + 1}.mat'
+                data = loadmat(path)
 
-
-
+                print(data['__header__'], data['__version__'], data['__globals__'], data['value'])
